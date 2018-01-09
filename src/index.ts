fe5f357205e8dd7b7837200e5d77890fb2b2cdda
simon <simonbbyrne@gmail.com>
2018-01-09 20:18:34 +0000

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+
+
+function component() {
+    var element = document.createElement('pre');
+
+    element.innerHTML = [
+        'Hello', 'webpack!!'
+    ].join('\n\n');
+
+    return element;
+}
+
+document.body.appendChild(component());
---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Production'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
